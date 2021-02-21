ab4a801a717935e57700a0092c8ca26718897167
John <davis.p.john@gmail.com>
2021-02-21 17:25:47 -0600

    Adds that content view for sure.

---
+++ TallGrass/ContentView.swift
@@ -0,0 +1,52 @@
+//
+//  ContentView.swift
+//  ScryOne
+//
+//  Created by John Davis on 1/17/21.
+//
+
+import SwiftUI
+
+extension URL: Identifiable {
+    public var id: String {
+        self.absoluteString
+    }
+}
+
+struct ContentView: View {
+    let columns = [ GridItem(.adaptive(minimum: 300, maximum: 600)) ]
+    
+    var body: some View {
+        ScrollView {
+            LazyVGrid(columns: columns, alignment: .center) {
+                VStack {
+                    OnboardingCard()
+                    Spacer()
+                }
+                
+                VStack {
+                    StepOneCard()
+                    Spacer()
+                }
+                
+                VStack {
+                    StepTwoCard()
+                    Spacer()
+                }
+                VStack {
+                    StepThreeCard()
+                    Spacer()
+                }
+                
+            }
+            .padding()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            
+    }
+}
